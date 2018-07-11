e31eea301428c0a3ab39631aad030cef3bb316ae
li_xiaoteng <li_xiaoteng@uxsino.com>
2018-07-11 09:59:53 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { NgZorroAntModule } from 'ng-zorro-antd';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single, radarData, pieData, lineData } from './data';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  single: any[];
+  radarData: any[];
+  pieData: any[];
+  lineData: any[];
+
+  view: any[] = [600, 400];
+  horizontalView: any[] = [700, 200];
+  pieView: any[] = [700, 300];
+  radarView: any[] = [700, 700];
+  lineView: any[] = [700, 300];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yaxisLabel = 'Population';
+
+  animations = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { single});
+    Object.assign(this, { radarData});
+    Object.assign(this, { pieData});
+    Object.assign(this, { lineData});
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
